29dea24cafe8e2ee9f2c869a67c5b781bad05245
s24267-pj <92219460+s24267-pj@users.noreply.github.com>
2024-12-17 23:52:30 +0100

    Add files via upload

---
+++ Zjazd4/src/main/java/org/example/Order.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Order {
+    private String product;
+    private int quantity;
+
+    public Order(String product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
