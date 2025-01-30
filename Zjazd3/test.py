faae044b7f090d53999b96a8bdb81cfe5ed87639
s24267-pj <92219460+s24267-pj@users.noreply.github.com>
2025-01-30 14:20:31 +0100

    Zmiany do zdobycia pkt
    - Plansza nie jest generowana "z ręki", a z generate_board w main.py
    - Poprawione i dodane testy

--- Zjazd3/test.py
+++ Zjazd3/test.py
@@ -1,48 +1,71 @@
 import unittest
-from main import move_up, move_down, move_left, move_right
+from main import generate_board, move, is_path_possible
 
 
 class TestGameLogic(unittest.TestCase):
 
     def setUp(self):
-        self.board = [
-            [' ', 'X', ' ', ' ', ' '],
-            [' ', ' ', 'X', ' ', ' '],
-            [' ', ' ', ' ', ' ', 'X'],
-            ['X', ' ', ' ', 'X', ' '],
-            [' ', ' ', ' ', ' ', ' ']
+        self.board, self.start_position, self.stop_position = generate_board(5, 5)
+        self.player_position = self.start_position
+
+    def test_player_cannot_move_outside(self):
+        self.player_position = (0, 2)
+        self.assertEqual(move(self.player_position, "up", self.board), (0, 2),
+                         "Gracz nie powienien poruszyć się w górę.")
+
+        self.player_position = (2, 0)
+        self.assertEqual(move(self.player_position, "left", self.board), (2, 0),
+                         "Gracz nie powienien poruszyć się w lewo.")
+
+        self.player_position = (4, 2)
+        self.assertEqual(move(self.player_position, "down", self.board), (4, 2),
+                         "Gracz nie powienien poruszyć się w dół")
+
+        self.player_position = (2, 4)
+        self.assertEqual(move(self.player_position, "right", self.board), (2, 4),
+                         "Gracz nie powienien poruszyć się w prawo")
+
+    def test_player_cannot_move_in_obstacle(self):
+        self.board[1][1] = 'X'
+        self.player_position = (1, 0)
+        self.assertEqual(move(self.player_position, "right", self.board), (1, 0),
+                         "Gracz nie powinien wejść na przeszkodę.")
+
+    def test_player_can_move(self):
+        self.board = [[" " for _ in range(5)] for _ in range(5)]
+        self.player_position = (1, 1)
+        self.board[1][1] = "P"
+
+        self.assertEqual(move(self.player_position, "up", self.board), (0, 1), "Gracz nie może poruszyć się w górę.")
+        self.assertEqual(move(self.player_position, "down", self.board), (2, 1), "Gracz nie może poruszyć się w dół.")
+        self.assertEqual(move(self.player_position, "left", self.board), (1, 0), "Gracz nie może poruszyć się w lewo.")
+        self.assertEqual(move(self.player_position, "right", self.board), (1, 2),
+                         "Gracz nie może poruszyć się w prawo.")
+
+    def test_start_and_stop_positions_are_different(self):
+        self.assertNotEqual(self.start_position, self.stop_position, "Pozycje START i STOP powinny być różne")
+
+    def test_player_initial_position_equals_start_position(self):
+        self.assertEqual(self.player_position, self.start_position,
+                         "Początkowa pozycja gracza powinna być równa pozycji START")
+
+    def test_player_reaches_stop(self):
+        self.player_position = self.stop_position
+        self.assertEqual(self.player_position, self.stop_position, "Gracz powinien dotrzeć do pola STOP")
+
+    def test_no_path_to_stop(self):
+        start_x, start_y = self.start_position
+        potential_obstacles = [
+            (start_x - 1, start_y), (start_x + 1, start_y),
+            (start_x, start_y - 1), (start_x, start_y + 1)
         ]
+        self.board = [[" " for _ in range(5)] for _ in range(5)]
+        for x, y in potential_obstacles:
+            if 0 <= x < 5 and 0 <= y < 5:
+                self.board[x][y] = 'X'
 
-    def test_move_up(self):
-        self.assertEqual(move_up((3, 2), self.board), (2, 2), "Nieprawidowy ruch w górę: przeszkoda.")
-        self.assertEqual(move_up((0, 0), self.board), (0, 0), "Nieprawidowy ruch w górę: wyjście poza planszę.")
-
-    def test_move_down(self):
-        self.assertEqual(move_down((3, 4), self.board), (4, 4), "Nieprawidowy ruch w dół: przeszkoda.")
-        self.assertEqual(move_down((4, 4), self.board), (4, 4), "Nieprawidłowy ruch w dół: : wyjście poza planszę.")
-
-    def test_move_left(self):
-        self.assertEqual(move_left((2, 2), self.board), (2, 1), "Nieprawidowy ruch w lewo: przeszkoda.")
-        self.assertEqual(move_left((3, 0), self.board), (3, 0), "Nieprawidłowy ruch w lewo: wyjście poza planszę.")
-
-    def test_move_right(self):
-        self.assertEqual(move_right((2, 2), self.board), (2, 3), "Nieprawidowy ruch w prawo: przeszkoda.")
-        self.assertEqual(move_right((2, 4), self.board), (2, 4), "Nieprawidłowy ruch w prawo: wyjście poza planszę.")
-
-    def test_reach_goal(self):
-        board = [
-            [' ', ' ', ' ', ' ', 'B'],
-            [' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ']
-        ]
-        start = (0, 3)
-        stop = (0, 4)
-
-        position = move_right(start, board)
-
-        self.assertEqual(position, stop, "Gracz nie dotarł do mety.")
+        self.assertFalse(is_path_possible(self.start_position, self.stop_position, self.board),
+                         "Nie powinno być ścieżki do STOP")
 
 
 if __name__ == "__main__":
